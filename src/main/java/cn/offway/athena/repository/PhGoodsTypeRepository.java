5dd2aac7faae95585b8865d05eaf990cf2ea11a7
XiaZhengXin <xzx@xiazhengxin.name>
2019-09-16 17:31:24 +0800

    迁移 offway 后台的 商品 类别、类目 实体类及业务接口、接口实现类 至 showRoom....
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

---
+++ src/main/java/cn/offway/athena/repository/PhGoodsTypeRepository.java
@@ -0,0 +1,14 @@
+package cn.offway.athena.repository;
+
+import cn.offway.athena.domain.PhGoodsType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+/**
+ * 商品类别Repository接口
+ *
+ * @author wn
+ * @version $v: 1.0.0, $time:2019-04-04 15:18:00 Exp $
+ */
+public interface PhGoodsTypeRepository extends JpaRepository<PhGoodsType, Long>, JpaSpecificationExecutor<PhGoodsType> {
+}
---
+++ src/main/java/cn/offway/athena/service/PhGoodsCategoryService.java
@@ -0,0 +1,32 @@
+package cn.offway.athena.service;
+
+import cn.offway.athena.domain.PhGoodsCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/**
+ * 商品类目Service接口
+ *
+ * @author wn
+ * @version $v: 1.0.0, $time:2019-04-04 15:18:00 Exp $
+ */
+public interface PhGoodsCategoryService {
+
+    PhGoodsCategory save(PhGoodsCategory phGoodsCategory);
+
+    PhGoodsCategory findOne(Long id);
+
+    Page<PhGoodsCategory> findAll(Pageable pageable);
+
+    List<PhGoodsCategory> findAll();
+
+    void del(Long id);
+
+    List<PhGoodsCategory> findByPid(Long pid);
+
+    Page<PhGoodsCategory> findByPid(Long pid, Pageable pageable);
+
+    void delByPid(Long pid);
+}
