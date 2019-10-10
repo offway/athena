d2d8aa8e8a1776fdfca440b40b7a65ea3a08ad83
XiaZhengXin <xzx@xiazhengxin.name>
2019-10-10 15:39:09 +0800

    banner 历史高低排名 查看按钮加入。。。 交换字段以避免spring 主键数据复用。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/main/java/cn/offway/athena/repository/PhBannerHistoryRepository.java
+++ src/main/java/cn/offway/athena/repository/PhBannerHistoryRepository.java
@@ -14,6 +14,6 @@ import java.util.List;
  * @version $v: 1.0.0, $time:2019-10-10 14:00:40 Exp $
  */
 public interface PhBannerHistoryRepository extends JpaRepository<PhBannerHistory, Long>, JpaSpecificationExecutor<PhBannerHistory> {
-    @Query(nativeQuery = true, value = "SELECT count(`banner_id`) as id,`banner`,`banner_id`,`banner_img`,`create_time`,`begin_time`,`end_time` FROM phsr_dev.ph_banner_history group by banner_id order by count(`banner_id`) desc")
+    @Query(nativeQuery = true, value = "SELECT count(`banner_id`) as banner_id,`banner`,`banner_id` as id,`banner_img`,`create_time`,`begin_time`,`end_time` FROM ph_banner_history group by banner_id order by count(`banner_id`) desc")
     List<PhBannerHistory> listRank();
 }
